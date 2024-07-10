f9cbc16f499bbdf94c5f248e5713ba20d60fe06a
jmbae <jmbae@codegrove.co.kr>
2024-07-10 13:13:08 +0900

    Test 함수 작성

---
+++ Albertos/AlbertosTests/MenuGroupingTests.swift
@@ -0,0 +1,24 @@
+//
+//  AlbertosTests.swift
+//  AlbertosTests
+//
+//  Created by Jungman Bae on 7/10/24.
+//
+
+import XCTest
+@testable import Albertos
+
+
+final class MenuGroupingTests: XCTestCase {
+    func testMenuWithManyCategoriesReturnsOneSectionPerCategory() {
+        
+    }
+    
+    func testMenuWithOneCategoryReturnOneSection() {
+        
+    }
+    
+    func testEmptyMenuReturnsEmptySections() {
+        
+    }
+}
