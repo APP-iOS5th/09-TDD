cf4aed821b7dd17c8ca8ed49855a6c3b530594b6
jmbae <jmbae@codegrove.co.kr>
2024-07-12 15:26:03 +0900

    Spy 를 이용해서 order 전달 테스트

---
+++ Albertos/AlbertosTests/ViewModelsTests/OrderDetailViewModelTests.swift
@@ -0,0 +1,28 @@
+//
+//  OrderDetailViewModelTests.swift
+//  AlbertosTests
+//
+//  Created by Jungman Bae on 7/12/24.
+//
+
+import XCTest
+
+@testable import Albertos
+
+final class OrderDetailViewModelTests: XCTestCase {
+    
+    func testCheckoutButtonTappedStartsPaymentProcessingFlow() {
+        // Arrange
+        let orderController = OrderController()
+        orderController.addToOrder(item: .fixture(name: "name", price: 1.0))
+        orderController.addToOrder(item: .fixture(name: "other name", price: 2.0))
+        
+        let paymentProcessingSpy = PaymentProcessingSpy()
+        let viewModel = OrderDetail.ViewModel(
+            orderController: orderController,
+            paymentProcessor: paymentProcessingSpy)
+        viewModel.checkout()
+        XCTAssertEqual(paymentProcessingSpy.receivedOrder, orderController.order)
+    }
+
+}
