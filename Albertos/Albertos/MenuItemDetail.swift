0b9058d94d3202c690d22885abd8548ca196da05
jmbae <jmbae@codegrove.co.kr>
2024-07-12 13:15:33 +0900

    MenuItemDetail.ViewModel 테스트

---
+++ Albertos/Albertos/MenuItemDetail.swift
@@ -0,0 +1,18 @@
+//
+//  MenuItemDetail.swift
+//  Albertos
+//
+//  Created by Jungman Bae on 7/12/24.
+//
+
+import SwiftUI
+
+struct MenuItemDetail: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    MenuItemDetail()
+}
