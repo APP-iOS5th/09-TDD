7f13c3e6b913cb42f0122f0f42cccb983166df72
jmbae <jmbae@codegrove.co.kr>
2024-07-12 10:18:46 +0900

    MenuFetcher 작성

---
+++ Albertos/AlbertosTests/MenuFetcherTests.swift
@@ -0,0 +1,15 @@
+//
+//  MenuFetcherTests.swift
+//  AlbertosTests
+//
+//  Created by Jungman Bae on 7/12/24.
+//
+
+import XCTest
+
+@testable import Albertos
+
+final class MenuFetcherTests: XCTestCase {
+    func testWhenRequestSucceedsPublishesDecodedMenuItems() {}
+    func testWhenRequestFailsPublishesReceivedError() {}
+}
